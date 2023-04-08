7d5b184bd71be84c0d07a96dcd03f3c77b4427b7
Keiji-Mizumura <keijimizumura@icloud.com>
2023-04-09 05:40:34 +0900

    updated README.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './main.css';
 
-function App() {
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [advice, setAdvice] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    setIsLoading(true);
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => {
+        setAdvice(data.slip);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="advice">
+        <div className="advice-header">
+          <h3>ADVICE #{advice.id}</h3>
+        </div>
+        <div className="advice-content">{isLoading ? <div className='loading-spinner'><div></div></div> : <h1>“{advice.advice}”</h1>}</div>
+        <div className="divider">{/* image div */}</div>
+        <button className="btn" onClick={fetchAdvice}>
+          <img src="/images/icon-dice.svg" alt="dice" />
+        </button>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
